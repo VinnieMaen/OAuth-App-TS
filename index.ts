6f5f6defdeb3b3c8587c31a00afa8904b622bf4c
vincent <vincent_maenhout@hotmail.com>
2023-08-15 16:10:03 +0200

    Removed Ratelimit Headers

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ const port = process.env.PORT;
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 600,
-  standardHeaders: true,
+  standardHeaders: false,
   legacyHeaders: false,
 });
 
